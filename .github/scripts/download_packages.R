e3ba2fea44e655c12ea55cb1a17ab22214ca877d
ElinorK <16242091+elinork@users.noreply.github.com>
2023-03-23 13:44:30 -0400

    adding writing description files

--- .github/scripts/download_packages.R
+++ .github/scripts/download_packages.R
@@ -61,3 +61,18 @@ if ("sf"  %in% packages) {
 }
 
 # message("Completed downloading packages")
+
+library(tools)
+
+args <- commandArgs(trailingOnly = TRUE)
+
+# Test if a package destination directory was passed as an argument
+if (length(args) == 0 & dir.exists(args[1])) {
+  stop("Usage: R -f write_descriptions.R destination_directory", call. = FALSE)
+}
+
+tools::write_PACKAGES(dir = args[1], fields = NULL,
+               type = c("source"),
+               verbose = FALSE, unpacked = FALSE, subdirs = FALSE,
+               latestOnly = TRUE, addFiles = FALSE, rds_compress = "xz",
+               validate = FALSE)
