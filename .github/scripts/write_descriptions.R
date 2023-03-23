e238891ffa5497fc60a013ae3ddf736ffb83b7d3
ElinorK <16242091+elinork@users.noreply.github.com>
2023-03-23 09:12:22 -0400

    Adding copying libraries needed by sf

--- .github/scripts/write_descriptions.R
+++ .github/scripts/write_descriptions.R
@@ -1,4 +1,13 @@
 library(tools)
+
+args <- commandArgs(trailingOnly = TRUE)
+
+# Test if a package destination directory was passed as an argument
+if (length(args) == 0 & dir.exists(args[1])) {
+  stop("Usage: R -f write_descriptions.R destination_directory", \
+        call. = FALSE)
+}
+
 tools::write_PACKAGES(dir = args[1], fields = NULL,
                type = c("source"),
                verbose = FALSE, unpacked = FALSE, subdirs = FALSE,
