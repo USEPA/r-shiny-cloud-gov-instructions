5dfee278173867be82456479279e48e8c8ad6baa
ElinorK <16242091+elinork@users.noreply.github.com>
2023-03-24 17:55:10 -0400

    try cloudfoundry container

--- .github/scripts/extract_libraries.sh
+++ .github/scripts/extract_libraries.sh
@@ -3,7 +3,8 @@ if [ -d "/home/vcap" ]; then
         for L in $(ls r-lib); do
             tar -xf r-lib/$L -C /home/vcap/deps/0/r/lib
         done;
+        export GDAL_DATA=/home/vcap/deps/0/r/lib/gdal
     fi;
 fi;
 
-export GDAL_DATA=/home/vcap/deps/0/r/lib/gdal
+# /home/vcap/deps/0/apt/usr/share/gdal/2.2/
