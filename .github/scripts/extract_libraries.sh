d8392eb066010c684c5f199a53712fa099591b2c
ElinorK <16242091+elinork@users.noreply.github.com>
2023-03-25 11:12:08 -0400

    updating container

--- .github/scripts/extract_libraries.sh
+++ .github/scripts/extract_libraries.sh
@@ -1,9 +1,21 @@
+#!/bin/bash
+
 if [ -d "/home/vcap" ]; then 
     if [ -d "r-lib" ]; then 
         for L in $(ls r-lib); do
             tar -xf r-lib/$L -C /home/vcap/deps/0/r/lib
         done;
-        export GDAL_DATA=/home/vcap/deps/0/r/lib/gdal
+    fi;
+
+    if [ -d "r-usr-share" ]; then
+        if [ ! -d "/home/vcap/deps/0/r/usr" ]; then 
+            mkdir "/home/vcap/deps/0/r/usr"; 
+            mkdir "/home/vcap/deps/0/r/usr/share";
+        fi
+        for L in $(ls r-usr-share); do
+            tar -xf r-usr-share/$L -C /home/vcap/deps/0/r/usr/share
+        done;
+        export GDAL_DATA=/home/vcap/deps/0/r/usr/share/gdal/2.2
     fi;
 fi;
 
---
+++ .github/scripts/install_geospatial.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Taken from https://github.com/rocker-org/rocker-versioned2
+
+set -e
+
+# always set this for scripts but don't declare as ENV..
+export DEBIAN_FRONTEND=noninteractive
+
+## build ARGs
+NCPUS=${NCPUS:--1}
+
+# a function to install apt packages only if they are not installed
+function apt_install() {
+    if ! dpkg -s "$@" >/dev/null 2>&1; then
+        if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+            apt-get update
+        fi
+        apt-get install -y --no-install-recommends "$@"
+    fi
+}
+
+apt_install \
+    gdal-bin \
+    lbzip2 \
+    libfftw3-dev \
+    libgdal-dev \
+    libgeos-dev \
+    libgsl0-dev \
+    libgl1-mesa-dev \
+    libglu1-mesa-dev \
+    libhdf4-alt-dev \
+    libhdf5-dev \
+    libjq-dev \
+    libpq-dev \
+    libproj-dev \
+    libprotobuf-dev \
+    libnetcdf-dev \
+    libsqlite3-dev \
+    libssl-dev \
+    libudunits2-dev \
+    netcdf-bin \
+    postgis \
+    protobuf-compiler \
+    sqlite3 \
+    tk-dev \
+    unixodbc-dev
