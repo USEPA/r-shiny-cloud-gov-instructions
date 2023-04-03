14e9cf53c35c83bff62186dfb20533b2255378bd
ElinorK <16242091+elinork@users.noreply.github.com>
2023-04-03 09:12:45 -0400

    remove unneeded scripts

--- extract_libraries.sh
+++
@@ -1,9 +0,0 @@
-if [ -d "/home/vcap" ]; then 
-    if [ -d "r-lib" ]; then 
-        for L in $(ls r-lib); do
-            tar -xf r-lib/$L -C /home/vcap/deps/0/r/lib
-        done;
-    fi;
-fi;
-
-export GDAL_DATA=/home/vcap/deps/0/r/lib/gdal
